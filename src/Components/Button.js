5fc3bf8e5ef3c8e76a66e019c4eeae1cc842d778
StephenSchooley <schooley.sa@gmail.com>
2021-05-18 13:36:00 -0400

    app up to Firebase integration

---
+++ src/Components/Button.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+const Button = (props) => {
+
+    return (
+        <div>
+            <button className="buttons" onClick={props.toggleAddForm}>{props.text}</button>
+        </div>
+    )
+}
+
+export default Button
---
+++ src/Components/ScheduleItem.js
@@ -0,0 +1,14 @@
+import Item from './Item'
+
+const ScheduleItem = (props) => {
+
+    return (
+        <div>
+            {props.scheduleItems.map((item) => (
+                <Item id={item.id} item={item} deleteItem={props.deleteItem} />
+            ))}
+        </div>
+    )
+}
+
+export default ScheduleItem
